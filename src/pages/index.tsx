a68ed1ac56c45c1d6cece12634149bd8d31533a7
Maick Souza <maick_a_s@msn.com>
2021-04-24 19:09:58 -0300

    chore(web): global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <>
-      <h1 className={styles.title}>
-        <Head>
-          <title>Início | ig.news</title>
-        </Head>
-
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
         Maick Souza
       </h1>
     </>  
