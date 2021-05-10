642b615935108911298d30cf4edda0110ddfeb6e
Maick Souza <maick_a_s@msn.com>
2021-05-10 16:51:26 -0300

    chore(web): saving user at DB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.module.scss'
 export function SignInButon() {
   const [session] = useSession();
   
-  console.log(session)
-
   return session ? (
     <button
       type="button"
