fef560f05215fbc0f7932ed0a8fd99650b141842
Maick Souza <maick_a_s@msn.com>
2021-05-11 16:03:26 -0300

    feat(web): saving event data #2

--- src/pages/api/_lib/manageSubcription.ts
+++ src/pages/api/_lib/manageSubcription.ts
@@ -1,6 +1,36 @@
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+import { stripe } from '../../../services/stripe';
+
 export async function saveSubcription(
   subcriptionId: string,
   customerId: string,
 ) {
-  console.log(subcriptionId, customerId)
+  const userRef = await fauna.query(
+    q.Select(
+      'ref',
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  )
+
+  const subscription = await stripe.subscriptions.retrieve(subcriptionId)
+
+  const subcriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
+
+  await fauna.query(
+    q.Create(
+      q.Collection('subcriptions'),
+      { data: subcriptionData }
+    )
+  )
 }
